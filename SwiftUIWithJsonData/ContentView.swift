79d3f62aad5507ef48f2a685edaca548de0588da
Mohan Chanddrasekar <psgtech.mohan@gmail.com>
2020-07-14 21:11:51 +0200

    Initial Commit

---
+++ SwiftUIWithJsonData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWithJsonData
+//
+//  Created by Mohan Chandrasekar  on 2020-07-14.
+//  Copyright © 2020 Mohan Chandrasekar . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
